ee62200d52de8ae4d0985ce48a097ba980b8440d
Niccolò Albanese <nick_albanese@virgilio.it>
2024-12-09 16:56:23 +0100

    okok

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -9,10 +9,15 @@ const Job = ({ data }) => {
       className="mx-0 mt-3 p-3"
       style={{ border: '1px solid #00000033', borderRadius: 4 }}
     >
-      <Col xs={3}>
+      <Col xs={3} className="d-flex justify-content-between">
         <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9} className="d-flex justify-content-around">
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
         <Button
-          className="d-flex align-items-center"
+          className="d-flex align-items-center m-2"
           onClick={() => {
             // da questo onClick scaturiremo un cambio di stato!
             // per farlo dobbiamo fare il "dispatch" di una "action"
@@ -29,11 +34,6 @@ const Job = ({ data }) => {
         >
           <span className="me-2">AGGIUNGI AI PREFERITI</span>
         </Button>
-      </Col>
-      <Col xs={9}>
-        <a href={data.url} target="_blank" rel="noreferrer">
-          {data.title}
-        </a>
       </Col>
     </Row>
   )
